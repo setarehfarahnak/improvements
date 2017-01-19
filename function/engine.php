846502acade1a6c185df4cac19f9587b92defe0e
Setareh Farahnak <setareh.farahnak@gmail.com>
2017-01-19 11:16:34 +0100

    started backend

--- function/engine.php
+++ function/engine.php
@@ -1,7 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Setareh
- * Date: 2017-01-17
- * Time: 11:59
- */
+echo 'hej';
+$localhost = "localhost";
+$dbname = "improvements";
+$user = "root";
+$pass = "root";
+
+$pdo = new PDO("mysql:charset=utf8; host=$localhost; dbname=$dbname", $user, $pass);
+
+
+if (is_ajax()) {
+    if (isset($_POST["improvment"]) && !empty($_POST["improvment"]) && isset($_POST["team"]) && !empty($_POST["team"])) { //Checks if action value exists
+        improvement_create();
+    }
+}
+
+//Function to check if the request is an AJAX request
+function is_ajax() {
+    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+}
+
+function improvement_create(){
+
+//    $sql = "INSERT INTO $dbname ('improv_content','improv_team')
+//            VALUES (\"".$_POST['improvment']."\",
+//                     \"".$_POST['team']."\",
+//                     )";
+//
+//
+//    $stmt = $pdo->query($sql);
+
+    $return = $_POST;
+    $return["json"] = json_encode($return);
+    echo json_encode($return);
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
-/**
- * Created by Setareh on 2017-01-17.
- */
+$ = jQuery;
+$(function(){
+    $(".js-ajax-php-json").submit(function() {
+
+        var improvement = document.getElementById('improvContent').value;
+        var team = document.getElementById('improvTeam').value;
+        $('#myModal').modal('toggle');
+
+        var data = {
+            "improvment": improvement, 'team':team
+        };
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "function/engine.php", //Relative or absolute path to response.php file
+            data: data,
+            success: function (data) {
+                document.getElementById('improvContent').value = null;
+                alert("Form submitted successfully.\nReturned json: " + data["json"]);
+            }
+        });
+        return false;
+    });
+
+});
