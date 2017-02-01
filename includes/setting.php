75896dd0066fb52f9fcada584f258deaf3d94da7
Setareh Farahnak <setareh.farahnak@gmail.com>
2017-02-01 10:30:43 +0100

    db update

--- includes/setting.php
+++ includes/setting.php
@@ -1,10 +1,6 @@
 <?php
-
-
-$localhost = "localhost";
-$dbname = "improvements";
-$user = "root";
-$pass = "root";
-
-
+$localhost = "improvements-211740.mysql.binero.se";
+$dbname = "211740-improvements";
+$user = "211740_yz78662";
+$pass = "yz78662211740";
 ?>
